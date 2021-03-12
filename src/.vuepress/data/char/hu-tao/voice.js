605418cd413791869901315321d47cbc0bdc3ae2
SleepyAsh0191 <whksoft@gmail.com>
2021-03-12 19:33:25 +0800

    完善胡桃语音

--- src/.vuepress/data/char/hu-tao/voice.js
+++ src/.vuepress/data/char/hu-tao/voice.js
@@ -8,6 +8,457 @@ export default [
             jp: '/assets/voice/char/hu-tao/hello/jp.ogg', // 日配
             en: '/assets/voice/char/hu-tao/hello/en.ogg', // 英配
             kr: '/assets/voice/char/hu-tao/hello/kr.ogg' // 韩配
-        }
+        },
+        text: '唷，找本堂主有何贵干呀？嗯？你不知道吗，往生堂第七十七代堂主就是胡桃我啦！嘶，不过瞧你的模样，容光焕发。身体健康，嗯…想必是为了工作以外的事来找我，对吧？' //语音内容
+    },
+    {
+        title: '闲聊·往生堂',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/chat/wangsheng/1101_cn.wav',
+            jp: '/assets/voice/char/hu-tao/chat/wangsheng/1101_jp.wav',
+            en: '/assets/voice/char/hu-tao/chat/wangsheng/1101_en.wav',
+            kr: '/assets/voice/char/hu-tao/chat/wangsheng/1101_kr.wav'
+        },
+        text: '不如去我那喝杯茶？嘿~'
+    },
+    {
+        title: '闲聊·客户',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/chat/customer/1102_cn.wav',
+            jp: '/assets/voice/char/hu-tao/chat/customer/1102_jp.wav',
+            en: '/assets/voice/char/hu-tao/chat/customer/1102_en.wav',
+            kr: '/assets/voice/char/hu-tao/chat/customer/1102_kr.wav'
+        },
+        text: '一个客户，两个客户，三个客户…'
+    },
+    {
+        title: '闲聊·偷懒',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/chat/break/1103_cn.wav',
+            jp: '/assets/voice/char/hu-tao/chat/break/1103_jp.wav',
+            en: '/assets/voice/char/hu-tao/chat/break/1103_en.wav',
+            kr: '/assets/voice/char/hu-tao/chat/break/1103_kr.wav'
+        },
+        text: '太阳出来我晒太阳，月亮出来我晒月亮咯~'
+    },
+    {
+        title: '下雨的时候…',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/weather/rain/1201_cn.wav',
+            jp: '/assets/voice/char/hu-tao/weather/rain/1201_jp.wav',
+            en: '/assets/voice/char/hu-tao/weather/rain/1201_en.wav',
+            kr: '/assets/voice/char/hu-tao/weather/rain/1201_kr.wav'
+        },
+        text: '哎呀…麻烦咯。'
+    },
+    {
+        title: '打雷的时候…',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/weather/thunder/1202_cn.wav',
+            jp: '/assets/voice/char/hu-tao/weather/thunder/1202_jp.wav',
+            en: '/assets/voice/char/hu-tao/weather/thunder/1202_en.wav',
+            kr: '/assets/voice/char/hu-tao/weather/thunder/1202_kr.wav'
+        },
+        text: '打雷啦，起身啦。'
+    },
+    {
+        title: '下雪的时候…',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/weather/snow/1203_cn.wav',
+            jp: '/assets/voice/char/hu-tao/weather/snow/1203_jp.wav',
+            en: '/assets/voice/char/hu-tao/weather/snow/1203_en.wav',
+            kr: '/assets/voice/char/hu-tao/weather/snow/1203_kr.wav'
+        },
+        text: '哇，是雪！'
+    },
+    {
+        title: '起风的时候…',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/weather/windy/1204_cn.wav',
+            jp: '/assets/voice/char/hu-tao/weather/windy/1204_jp.wav',
+            en: '/assets/voice/char/hu-tao/weather/windy/1204_en.wav',
+            kr: '/assets/voice/char/hu-tao/weather/windy/1204_kr.wav'
+        },
+        text: '嗯，真舒服'
+    },
+    {
+        title: '刮大风了…',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/weather/windblow/1205_cn.wav',
+            jp: '/assets/voice/char/hu-tao/weather/windblow/1205_jp.wav',
+            en: '/assets/voice/char/hu-tao/weather/windblow/1205_en.wav',
+            kr: '/assets/voice/char/hu-tao/weather/windblow/1205_kr.wav'
+        },
+        text: '欸？…唉，帽子都要吹飞了。'
+    },
+    {
+        title: '早上好…',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/time/morning/1001_cn.wav',
+            jp: '/assets/voice/char/hu-tao/time/morning/1001_jp.wav',
+            en: '/assets/voice/char/hu-tao/time/morning/1001_en.wav',
+            kr: '/assets/voice/char/hu-tao/time/morning/1001_kr.wav'
+        },
+        text: '嗯~早起身体好，晚睡人会飘。'
+    },
+    {
+        title: '中午好…',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/time/afternoon/1002_cn.wav',
+            jp: '/assets/voice/char/hu-tao/time/afternoon/1002_jp.wav',
+            en: '/assets/voice/char/hu-tao/time/afternoon/1002_en.wav',
+            kr: '/assets/voice/char/hu-tao/time/afternoon/1002_kr.wav'
+        },
+        text: '哟！中午好呀，吃了吗？'
+    },
+    {
+        title: '晚上好…',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/time/evening/1003_cn.wav',
+            jp: '/assets/voice/char/hu-tao/time/evening/1003_jp.wav',
+            en: '/assets/voice/char/hu-tao/time/evening/1003_en.wav',
+            kr: '/assets/voice/char/hu-tao/time/evening/1003_kr.wav'
+        },
+        text: '嘻嘻，月亮出来咯~咱也出门吧。'
+    },
+    {
+        title: '晚安…',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/time/night/1004_cn.wav',
+            jp: '/assets/voice/char/hu-tao/time/night/1004_jp.wav',
+            en: '/assets/voice/char/hu-tao/time/night/1004_en.wav',
+            kr: '/assets/voice/char/hu-tao/time/night/1004_kr.wav'
+        },
+        text: '哦，困了吗？那你好好休息，我一个人四处转转。'
+    },
+    {
+        title: '关于胡桃自己·名字…',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/about/self/2001_cn.wav',
+            jp: '/assets/voice/char/hu-tao/about/self/2001_jp.wav',
+            en: '/assets/voice/char/hu-tao/about/self/2001_en.wav',
+            kr: '/assets/voice/char/hu-tao/about/self/2001_kr.wav'
+        },
+        text: '胡桃的胡是胡吃海喝的胡，胡桃的桃却不是淘气的淘！嘿嘿…不、不好笑吗？'
+    },
+    {
+        title: '关于胡桃自己·拿手好戏…',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/about/self/2002_cn.wav',
+            jp: '/assets/voice/char/hu-tao/about/self/2002_jp.wav',
+            en: '/assets/voice/char/hu-tao/about/self/2002_en.wav',
+            kr: '/assets/voice/char/hu-tao/about/self/2002_kr.wav'
+        },
+        text: '变个戏法给你看，瞧好了哦…火！然后…嚯！火蝴蝶！呼，飞啦——哈哈。'
+    },
+    {
+        title: '关于我们·好奇心',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/about/us/2003_cn.wav',
+            jp: '/assets/voice/char/hu-tao/about/us/2003_jp.wav',
+            en: '/assets/voice/char/hu-tao/about/us/2003_en.wav',
+            kr: '/assets/voice/char/hu-tao/about/us/2003_kr.wav'
+        },
+        text: '你到处旅行，一定见多识广。有空也讲讲你自己的故事吧。'
+    },
+    {
+        title: '关于我们·夜晚',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/about/us/2004_cn.wav',
+            jp: '/assets/voice/char/hu-tao/about/us/2004_jp.wav',
+            en: '/assets/voice/char/hu-tao/about/us/2004_en.wav',
+            kr: '/assets/voice/char/hu-tao/about/us/2004_kr.wav'
+        },
+        text: '白天你尽管到处跑，晚上可得小心点。我不在的时候，务必谨慎行动。'
+    },
+    {
+        title: '关于我们·帮手',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/about/us/2005_cn.wav',
+            jp: '/assets/voice/char/hu-tao/about/us/2005_jp.wav',
+            en: '/assets/voice/char/hu-tao/about/us/2005_en.wav',
+            kr: '/assets/voice/char/hu-tao/about/us/2005_kr.wav'
+        },
+        text: '需要帮手吗？需要帮手吗？我来啦！若你需要帮助，胡桃我定当全力以赴，绝不推辞。'
+    },
+    {
+        title: '关于我们·战斗',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/about/us/2006_cn.wav',
+            jp: '/assets/voice/char/hu-tao/about/us/2006_jp.wav',
+            en: '/assets/voice/char/hu-tao/about/us/2006_en.wav',
+            kr: '/assets/voice/char/hu-tao/about/us/2006_kr.wav'
+        },
+        text: '战斗是件麻烦事，对我来说，它不是目的而是手段。用手段来达到目的，用战斗来维护不愿让步的东西。就这方面而言，我和你是一样的。'
+    },
+    {
+        title: '关于「神之眼」',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/about/vision/3001_cn.wav',
+            jp: '/assets/voice/char/hu-tao/about/vision/3001_jp.wav',
+            en: '/assets/voice/char/hu-tao/about/vision/3001_en.wav',
+            kr: '/assets/voice/char/hu-tao/about/vision/3001_kr.wav'
+        },
+        text: '「神之眼」…「神之眼」？唔，这东西，随缘就好。'
+    },
+    {
+        title: '有什么想要分享…',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/share/3002_cn.wav',
+            jp: '/assets/voice/char/hu-tao/share/3002_jp.wav',
+            en: '/assets/voice/char/hu-tao/share/3002_en.wav',
+            kr: '/assets/voice/char/hu-tao/share/3002_kr.wav'
+        },
+        text: '受人之托，忠人之事。我们往生堂性质特殊，肩负着双倍责任，所以，一定要让两个世界的人都满意才行。'
+    },
+    {
+        title: '感兴趣的见闻…',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/interest/3003_cn.wav',
+            jp: '/assets/voice/char/hu-tao/interest/3003_jp.wav',
+            en: '/assets/voice/char/hu-tao/interest/3003_en.wav',
+            kr: '/assets/voice/char/hu-tao/interest/3003_kr.wav'
+        },
+        text: '有些花草只长在阴气重且月光多的地方，你见过吗？没有？那下次带你见识见识。'
+    },
+    {
+        title: '关于钟离…',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/about/char/zhongli/4001_cn.wav',
+            jp: '/assets/voice/char/hu-tao/about/char/zhongli/4001_jp.wav',
+            en: '/assets/voice/char/hu-tao/about/char/zhongli/4001_en.wav',
+            kr: '/assets/voice/char/hu-tao/about/char/zhongli/4001_kr.wav'
+        },
+        text: ['钟离长得那么年轻，性格倒像是个老古董，什么都懂，却什么都不关心，他该不会…？嘻嘻…', '唉，算了，随他去吧。']
+    },
+    {
+        title: '关于七七…',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/about/char/qiqi/4002_cn.wav',
+            jp: '/assets/voice/char/hu-tao/about/char/qiqi/4002_jp.wav',
+            en: '/assets/voice/char/hu-tao/about/char/qiqi/4002_en.wav',
+            kr: '/assets/voice/char/hu-tao/about/char/qiqi/4002_kr.wav'
+        },
+        text: '看到七七了吗？快告诉我她在哪，我要把她藏起来，嘿！'
+    },
+    {
+        title: '关于白术…',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/about/char/baizhu/4003_cn.wav',
+            jp: '/assets/voice/char/hu-tao/about/char/baizhu/4003_jp.wav',
+            en: '/assets/voice/char/hu-tao/about/char/baizhu/4003_en.wav',
+            kr: '/assets/voice/char/hu-tao/about/char/baizhu/4003_kr.wav'
+        },
+        text: '白术？哼，麻烦的家伙。'
+    },
+    {
+        title: '关于凝光…',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/about/char/ningguang/4004_cn.wav',
+            jp: '/assets/voice/char/hu-tao/about/char/ningguang/4004_jp.wav',
+            en: '/assets/voice/char/hu-tao/about/char/ningguang/4004_en.wav',
+            kr: '/assets/voice/char/hu-tao/about/char/ningguang/4004_kr.wav'
+        },
+        text: '天权凝光，富甲一方；明眸善睐，桃羞杏让；目达耳通，百了千当！'
+    },
+    {
+        title: '关于北斗…',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/about/char/beidou/4005_cn.wav',
+            jp: '/assets/voice/char/hu-tao/about/char/beidou/4005_jp.wav',
+            en: '/assets/voice/char/hu-tao/about/char/beidou/4005_en.wav',
+            kr: '/assets/voice/char/hu-tao/about/char/beidou/4005_kr.wav'
+        },
+        text: '北斗姐可是璃月名人，她认不认识我没关系，我认识她就行。啊~真想和她交个朋友。'
+    },
+    {
+        title: '关于行秋…',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/about/char/xingqiu/4006_cn.wav',
+            jp: '/assets/voice/char/hu-tao/about/char/xingqiu/4006_jp.wav',
+            en: '/assets/voice/char/hu-tao/about/char/xingqiu/4006_en.wav',
+            kr: '/assets/voice/char/hu-tao/about/char/xingqiu/4006_kr.wav'
+        },
+        text: '听说行秋少爷在写书？话本还是怪谈呢？好想把平日遇见的杂事说给他听，写成书一定很好看吧。'
+    },
+    {
+        title: '关于重云…',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/about/char/chongyun/4007_cn.wav',
+            jp: '/assets/voice/char/hu-tao/about/char/chongyun/4007_jp.wav',
+            en: '/assets/voice/char/hu-tao/about/char/chongyun/4007_en.wav',
+            kr: '/assets/voice/char/hu-tao/about/char/chongyun/4007_kr.wav'
+        },
+        text: '纯阳正气，阴阳合一？啧啧，嘶…没想到世上真的有这种体质的人。'
+    },
+    {
+        title: '关于魈…',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/about/char/xiao/4008_cn.wav',
+            jp: '/assets/voice/char/hu-tao/about/char/xiao/4008_jp.wav',
+            en: '/assets/voice/char/hu-tao/about/char/xiao/4008_en.wav',
+            kr: '/assets/voice/char/hu-tao/about/char/xiao/4008_kr.wav'
+        },
+        text: '仙家的事，我怎么知道？哎呀呀，天机不可泄露，你就别问我啦。'
+    },
+    {
+        title: '关于刻晴…',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/about/char/keqing/4009_cn.wav',
+            jp: '/assets/voice/char/hu-tao/about/char/keqing/4009_jp.wav',
+            en: '/assets/voice/char/hu-tao/about/char/keqing/4009_en.wav',
+            kr: '/assets/voice/char/hu-tao/about/char/keqing/4009_kr.wav'
+        },
+        text: '「玉衡」刻晴啊，嘶…是个很难打交道的人呢。嘿嘿，瞧那一本正经的样子，真想逗逗她。哎呦，她应该不会用剑敲我的头吧？不会的吧？'
+    },
+    {
+        title: '关于香菱…',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/about/char/xiangling/4010_cn.wav',
+            jp: '/assets/voice/char/hu-tao/about/char/xiangling/4010_jp.wav',
+            en: '/assets/voice/char/hu-tao/about/char/xiangling/4010_en.wav',
+            kr: '/assets/voice/char/hu-tao/about/char/xiangling/4010_kr.wav'
+        },
+        text: '香菱可好玩了，一碰就跳，特别容易被唬住。做我们这行，最喜欢这种人喽。不过也得注意分寸，毕竟，锅巴会替香菱出头呢。'
+    },
+    {
+        title: '想要了解胡桃·其一',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/more/5001_cn.wav',
+            jp: '/assets/voice/char/hu-tao/more/5001_jp.wav',
+            en: '/assets/voice/char/hu-tao/more/5001_en.wav',
+            kr: '/assets/voice/char/hu-tao/more/5001_kr.wav'
+        },
+        text: '大丘丘病了，二丘丘瞧；三丘丘采药，四丘丘熬；五丘丘死了，六丘丘抬~欸…呼。'
+    },
+    {
+        title: '想要了解胡桃·其二',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/more/5002_cn.wav',
+            jp: '/assets/voice/char/hu-tao/more/5002_jp.wav',
+            en: '/assets/voice/char/hu-tao/more/5002_en.wav',
+            kr: '/assets/voice/char/hu-tao/more/5002_kr.wav'
+        },
+        text: '小巷派暗黑打油诗人，正是本人！'
+    },
+    {
+        title: '想要了解胡桃·其三',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/more/5003_cn.wav',
+            jp: '/assets/voice/char/hu-tao/more/5003_jp.wav',
+            en: '/assets/voice/char/hu-tao/more/5003_en.wav',
+            kr: '/assets/voice/char/hu-tao/more/5003_kr.wav'
+        },
+        text: '若要我说，往生堂的工作既不困难也不可怕，只是麻烦而已。可惜旁人理解不了，见到咱们夜里出工，总是有些惧怕。唉，是他们不懂啊。'
+    },
+    {
+        title: '想要了解胡桃·其四',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/more/5004_cn.wav',
+            jp: '/assets/voice/char/hu-tao/more/5004_jp.wav',
+            en: '/assets/voice/char/hu-tao/more/5004_en.wav',
+            kr: '/assets/voice/char/hu-tao/more/5004_kr.wav'
+        },
+        text: '我是「往生堂」第七十七代堂主，我爷爷是第七十五代。我经手的第一份工作，就是替爷爷办葬礼。是不是很意外啊？哎呀，不知不觉就在这行做了好些年，时间过得可真快。'
+    },
+    {
+        title: '想要了解胡桃·其五',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/more/5005_cn.wav',
+            jp: '/assets/voice/char/hu-tao/more/5005_jp.wav',
+            en: '/assets/voice/char/hu-tao/more/5005_en.wav',
+            kr: '/assets/voice/char/hu-tao/more/5005_kr.wav'
+        },
+        text: '阴阳有序，命运无常。死亡难以预测，却也有它的规矩。记住了，无论何时何地，无论有何原因，人都不该轻易挑衅「死」之一事。唯有认识它、尊重它，才能明白活着的价值。'
+    },
+    {
+        title: '胡桃的爱好…',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/personality/6001_cn.wav',
+            jp: '/assets/voice/char/hu-tao/personality/6001_jp.wav',
+            en: '/assets/voice/char/hu-tao/personality/6001_en.wav',
+            kr: '/assets/voice/char/hu-tao/personality/6001_kr.wav'
+        },
+        text: '天清海阔，皓月当空，此情此景，正适合作诗一首。'
+    },
+    {
+        title: '胡桃的烦恼…',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/personality/6002_cn.wav',
+            jp: '/assets/voice/char/hu-tao/personality/6002_jp.wav',
+            en: '/assets/voice/char/hu-tao/personality/6002_en.wav',
+            kr: '/assets/voice/char/hu-tao/personality/6002_kr.wav'
+        },
+        text: '唉…真想找些新乐子，无所事事比死亡可怕多了。'
+    },
+    {
+        title: '喜欢的食物…',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/personality/6003_cn.wav',
+            jp: '/assets/voice/char/hu-tao/personality/6003_jp.wav',
+            en: '/assets/voice/char/hu-tao/personality/6003_en.wav',
+            kr: '/assets/voice/char/hu-tao/personality/6003_kr.wav'
+        },
+        text: '♪啦啦啦啦~吃水煮鱼配虾饺啦~'
+    },
+    {
+        title: '讨厌的食物…',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/personality/6004_cn.wav',
+            jp: '/assets/voice/char/hu-tao/personality/6004_jp.wav',
+            en: '/assets/voice/char/hu-tao/personality/6004_en.wav',
+            kr: '/assets/voice/char/hu-tao/personality/6004_kr.wav'
+        },
+        text: '清心炒史莱姆凝液…呕…这是香菱对我的复仇吗？'
+    },
+    {
+        title: '生日',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/birthday/7001_cn.wav',
+            jp: '/assets/voice/char/hu-tao/birthday/7001_jp.wav',
+            en: '/assets/voice/char/hu-tao/birthday/7001_en.wav',
+            kr: '/assets/voice/char/hu-tao/birthday/7001_kr.wav'
+        },
+        text: '今夜天星璀璨，月相壮丽，想必是个好日子。嘶，待我一算…嗯~我知道我知道，是你的生辰！哎呀，真是个好日子。'
+    },
+    {
+        title: '突破的感受·起',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/ascension/8001_cn.wav',
+            jp: '/assets/voice/char/hu-tao/ascension/8001_jp.wav',
+            en: '/assets/voice/char/hu-tao/ascension/8001_en.wav',
+            kr: '/assets/voice/char/hu-tao/ascension/8001_kr.wav'
+        },
+        text: '哦呀？哦呀呀？'
+    },
+    {
+        title: '突破的感受·承',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/ascension/8002_cn.wav',
+            jp: '/assets/voice/char/hu-tao/ascension/8002_jp.wav',
+            en: '/assets/voice/char/hu-tao/ascension/8002_en.wav',
+            kr: '/assets/voice/char/hu-tao/ascension/8002_kr.wav'
+        },
+        text: '嗯~真不错，谢啦。'
+    },
+    {
+        title: '突破的感受·转',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/ascension/8003_cn.wav',
+            jp: '/assets/voice/char/hu-tao/ascension/8003_jp.wav',
+            en: '/assets/voice/char/hu-tao/ascension/8003_en.wav',
+            kr: '/assets/voice/char/hu-tao/ascension/8003_kr.wav'
+        },
+        text: '没想到没想到，胡桃我现在力如泉涌，心似雄鹰，非常有自信哦。'
+    },
+    {
+        title: '突破的感受·合',
+        voice: {
+            cn: '/assets/voice/char/hu-tao/ascension/8004_cn.wav',
+            jp: '/assets/voice/char/hu-tao/ascension/8004_jp.wav',
+            en: '/assets/voice/char/hu-tao/ascension/8004_en.wav',
+            kr: '/assets/voice/char/hu-tao/ascension/8004_kr.wav'
+        },
+        text: '力量若达到极限，接着考验的便是人心。多亏了你，我才能来到这片未曾踏足的领域。在这里，心和灵魂就像一片云彩…生老病死，天地万象，尽在吾辈。'
     }
 ]
