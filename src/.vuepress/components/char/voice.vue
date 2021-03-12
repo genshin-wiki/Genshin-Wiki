83b3a41efe015f2ef12f408069a5b038ba43d73c
真心 <ceo@iqianye.cn>
2021-03-12 23:15:31 +0800

    优化语音

--- src/.vuepress/components/char/voice.vue
+++ src/.vuepress/components/char/voice.vue
@@ -2,39 +2,32 @@
   <div>
     <el-tabs v-model="activeName">
       <el-tab-pane v-for="(item, key) in data" :label="item.title" :key="key" :name="`${key}`">
-        <table>
-          <tr>
-            <th>汉语</th>
-            <td>
-              <audio controls>
-                <source :src="$baseURL + item.voice.cn" />
-              </audio>
-            </td>
-          </tr>
-          <tr>
-            <th>日语</th>
-            <td>
-              <audio controls>
-                <source :src="$baseURL + item.voice.jp" />
-              </audio>
-            </td>
-          </tr>
-          <tr>
-            <th>英语</th>
-            <td>
-              <audio controls>
-                <source :src="$baseURL + item.voice.en" />
-              </audio>
-            </td>
-          </tr>
-          <tr>
-            <th>韩语</th>
-            <td>
-              <audio controls>
-                <source :src="$baseURL + item.voice.kr" />
-              </audio>
-            </td>
-          </tr>
+        <span>{{ item.text }}</span>
+        <table class="char-voice-table">
+          <tbody>
+            <tr>
+              <th class="char-voice-th">语言</th>
+              <th>汉语</th>
+              <th>日语</th>
+              <th>英语</th>
+              <td>韩语</td>
+            </tr>
+            <tr>
+              <th class="char-voice-th">播放</th>
+              <td>
+                <el-button type="primary" @click="play($baseURL + item.voice.cn)">播放</el-button>
+              </td>
+              <td>
+                <el-button type="primary" @click="play($baseURL + item.voice.jp)">播放</el-button>
+              </td>
+              <td>
+                <el-button type="primary" @click="play($baseURL + item.voice.en)">播放</el-button>
+              </td>
+              <td>
+                <el-button type="primary" @click="play($baseURL + item.voice.kr)">播放</el-button>
+              </td>
+            </tr>
+          </tbody>
         </table>
       </el-tab-pane>
     </el-tabs>
@@ -49,14 +42,27 @@ export default {
   data() {
     return {
       data: [],
-      activeName: '0'
+      activeName: '0',
+      audio: new Audio()
     }
   },
   created() {
     const result = require(`../../data/char/${this.name}/voice`)
     this.data = result.default
+  },
+  methods: {
+    play(url) {
+      this.audio.src = url
+      this.audio.play()
+    }
   }
 }
 </script>
 
-<style lang="stylus"></style>
+<style lang="stylus">
+.char-voice-table
+  table-layout: fixed
+  min-width: 100%
+.char-voice-th
+  min-width: 36px
+</style>
