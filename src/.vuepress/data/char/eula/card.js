629869e2a217548c8336a9b02be70bc4896ceb95
小坎坷 <723733393@qq.com>
2021-03-20 21:05:30 +0800

    优菈

---
+++ src/.vuepress/data/char/eula/card.js
@@ -0,0 +1,31 @@
+// 角色资料卡数据
+
+export default {
+  avatar: '/image/char/avatar/Eula.png', // 头像链接
+  name: '优菈', // 角色名称
+  prefix: '浪沫的旋舞', // 角色称号
+  by: '西风骑士团', // 所属
+  rank: 5, // 星级
+  element: '冰', // 元素(神之眼)
+  constell: '浪沫座', // 命之座
+  weapon: '双手剑', // 武器类型
+  cv: { // CV列表
+    cn: '子音', // 中配
+    jp: '未知', // 日配
+    en: '未知', // 英配
+    kr: '未知' // 韩配
+  },
+  desc: '古老家族出身的「浪花骑士」，西风骑士团游击小队队长。身为旧贵族后裔却加入了堪称死对头的西风骑士团，该事件至今仍是蒙德一大谜团。', // 角色简介
+  attributes: { // 基础属性
+    attackType: '近战', // 攻击类型
+    hp: 1030, // 生命值
+    attack: 27, // 攻击力
+    defense: 58, // 防御力
+    proficient: 0, // 元素精通
+    critRate: '5.0%', // 暴击率
+    critDamage: '50.0%', // 暴击伤害
+    heal: '0.0%', // 治疗加成
+    healMe: '0.0%', // 受治疗加成
+    recharge: '100%' // 元素充能效率
+  }
+}
---
+++ src/char/eula.md
@@ -0,0 +1,36 @@
+---
+title: 优菈
+categories:
+  - 角色
+  - 西风骑士团
+  - 冰
+  - 五星
+keywords:
+  - 物理
+description: '古老家族出身的「浪花骑士」，西风骑士团游击小队队长。身为旧贵族后裔却加入了堪称死对头的西风骑士团，该事件至今仍是蒙德一大谜团。'
+image: '/image/char/avatar/Eula.png'
+---
+
+## 信息
+
+<char-card name="eula"/>
+
+## 图片
+
+<char-image name="eula"/>
+
+## 等级
+
+<char-level name="eula"/>
+
+## 天赋
+
+<char-talent name="eula"/>
+
+## 故事
+
+<char-story name="eula"/>
+
+## 语音
+
+<char-voice name="eula"/>
