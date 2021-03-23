96eec857c090a8bc8a9ee95f15d65e0fe2800dbd
真心 <ceo@iqianye.cn>
2021-03-24 00:37:57 +0800

    可莉

---
+++ src/.vuepress/data/char/klee/card.js
@@ -0,0 +1,31 @@
+// 角色资料卡数据
+
+export default {
+  avatar: '/image/char/avatar/Klee.png', // 头像链接
+  name: '可莉', // 角色名称
+  prefix: '逃跑的太阳', // 角色称号
+  by: '西风骑士团', // 所属
+  rank: 5, // 星级
+  element: '火', // 元素(神之眼)
+  constell: '四叶草座', // 命之座
+  weapon: '法器', // 武器类型
+  cv: { // CV列表
+    cn: '花玲', // 中配
+    jp: '久野美咲', // 日配
+    en: '帕娜姆·巴苏', // 英配
+    kr: '房莲知' // 韩配
+  },
+  desc: '西风骑士团禁闭室的常客，蒙德的爆破大师。人称「逃跑的太阳」。', // 角色简介
+  attributes: { // 基础属性
+    attackType: '远程', // 攻击类型
+    hp: 801, // 生命值
+    attack: 47, // 攻击力
+    defense: 48, // 防御力
+    proficient: 0, // 元素精通
+    critRate: '5.0%', // 暴击率
+    critDamage: '50%', // 暴击伤害
+    heal: '0.0%', // 治疗加成
+    healMe: '0.0%', // 受治疗加成
+    recharge: '100%' // 元素充能效率
+  }
+}
---
+++ src/char/klee.md
@@ -0,0 +1,43 @@
+---
+title: 可莉
+categories:
+  - 角色
+  - 蒙德
+  - 火
+  - 五星
+tags:
+  - 火花骑士
+  - 蹦蹦炸弹
+  - 逃跑的太阳
+  - 放火烧山真君
+description: '西风骑士团禁闭室的常客，蒙德的爆破大师。人称「逃跑的太阳」。'
+image: '/image/char/avatar/Klee.png'
+---
+
+## 信息
+
+<char-card name="klee"/>
+
+## 图片
+
+<char-image name="klee"/>
+
+## 等级
+
+<char-level name="klee"/>
+
+## 天赋
+
+<char-talent name="klee"/>
+
+## 命之座
+
+<char-constell name="klee"/>
+
+## 故事
+
+<char-story name="klee"/>
+
+## 语音
+
+<char-voice name="klee"/>
